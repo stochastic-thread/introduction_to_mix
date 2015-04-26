7161671a89da8892e8032ce5672b9489b3fd03d7
ArthurColle <acolle@yandex.com>
2015-04-26 02:45:59 -0400

    Added a setup callback to KV.BucketTest

--- test/bucket/bucket_test.exs
+++ test/bucket/bucket_test.exs
@@ -1,12 +1,16 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link
+    {:ok, bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
     {:ok, bucket} = KV.Bucket.start_link
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
-
 end

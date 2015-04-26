15fe90270126bdb4c1fb34b8ae958611fff82017
ArthurColle <acolle@yandex.com>
2015-04-25 23:34:05 -0400

    First commit

---
+++ lib/kv/bucket.ex
@@ -0,0 +1,22 @@
+defmodule KV.Bucket do
+  @doc """
+  Starts a new bucket.
+  """
+  def start_link do
+    Agent.start_link(fn -> HashDict.new end)
+  end
+
+  @doc """
+  Gets a value from the `bucket` by `key`.
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &HashDict.get(&1, key))
+  end
+
+  @doc """
+  Puts the value for the given `key` in the `bucket`.
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, &HashDict.put(&1, key, value))
+  end
+end
